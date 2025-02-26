1792f9a9f4fd3863ccf0fd8c4138ca16f4b909d2
Wasim Akram <mr.saim516@gmail.com>
2025-02-26 14:49:16 +0500

    increased body text size

--- src/app/styles/theme.js
+++ src/app/styles/theme.js
@@ -23,10 +23,10 @@ const theme = createTheme({
       fontSize: "1rem",
     },
     body1: {
-      fontSize: "0.8rem",
+      fontSize: "1rem",
     },
     body2: {
-      fontSize: "0.6rem",
+      fontSize: "0.8rem",
     },
     subtitle1: {
       fontSize: "0.7rem",
